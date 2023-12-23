32d7e88dd021f4cbbee1fef063b04a08de55f132
uditisinha <uditisinha7@gmail.com>
2023-12-23 14:40:13 +0530

    fixed lol

--- extcintranet/settings.py
+++ extcintranet/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'extcintranet.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresqp_psycopg2',
-        'NAME': 'project_database',
-        'USER': 'uditi',
-        'PASSWORD': 'pass',
-        'HOST': 'killme.pythonanywhere.com',
-        'PORT': ''
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
