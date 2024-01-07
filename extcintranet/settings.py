d718da650a4ba6784bb1d97457e35e596c00d801
uditisinha <uditisinha7@gmail.com>
2024-01-07 18:33:17 +0530

    ok

--- extcintranet/settings.py
+++ extcintranet/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -113,7 +112,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS= [
-    BASE_DIR / 'static',
+    BASE_DIR / 'static'
 ]
 
 MEDIA_ROOT = BASE_DIR / 'media'
